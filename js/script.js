7a12efce2c52241ea6ce21ac1fbcc1326f877184
“erikaahlstrom” <info@erikasoldh.se>
2020-11-12 16:05:30 +0100

    ändrade upplösning på bild, fixat bugg med body element i mixin

--- js/script.js
+++ js/script.js
@@ -1,75 +1,79 @@
-/* ======MENU SHOW OR HIDE=====*/
-console.log("hello");
-// whole menu bar + x icon
-let navMenu = document.getElementById("nav-menu")
-// hamburger icon
-let toggleMenu = document.getElementById("nav-toggle")
-// x icon
-let closeMenu = document.getElementById("nav-close")
+   document.addEventListener("DOMContentLoaded", () => {
 
-// show whole manu bar
-toggleMenu.addEventListener("click", (e) => {
-    navMenu.classList.add("show")
-})      
+   /* ======MENU SHOW OR HIDE=====*/
+    console.log("hello");
+    // whole menu bar + x icon
+    let navMenu = document.getElementById("nav-menu")
+    // hamburger icon
+    let toggleMenu = document.getElementById("nav-toggle")
+    // x icon
+    let closeMenu = document.getElementById("nav-close")
 
-// hide whole manu bar
-closeMenu.addEventListener("click", (e) => {
-    navMenu.classList.remove("show")
-})   
+    // show whole manu bar
+    toggleMenu.addEventListener("click", (e) => {
+        navMenu.classList.add("show")
+    })      
 
-/* ======REMOVE MENU=====*/
-// link item
-const navLink = document.querySelectorAll(".nav__link");
+    // hide whole manu bar
+    closeMenu.addEventListener("click", (e) => {
+        navMenu.classList.remove("show")
+    })   
 
-function linkAction() {
-    navMenu.classList.remove("show");
-}
+    /* ======REMOVE MENU=====*/
+    // link item
+    const navLink = document.querySelectorAll(".nav__link");
 
-navLink.forEach(n => n.addEventListener("click", linkAction));
+    function linkAction() {
+        navMenu.classList.remove("show");
+    }
 
-/* ======SCROLL ACTIVE LINK=====*/
-const sections = document.querySelectorAll("section[id]")
+    navLink.forEach(n => n.addEventListener("click", linkAction));
 
-// activate "scrollspy" 
-window.addEventListener("scroll", scrollActive)
+    /* ======SCROLL ACTIVE LINK=====*/
+    const sections = document.querySelectorAll("section[id]")
 
-//Keep track of where user is, makes current nav link active
-function scrollActive() {
-    // Scrollamount from user
-    let scrollY = window.pageYOffset;
-    
-    sections.forEach(current => {
+    // activate "scrollspy" 
+    window.addEventListener("scroll", scrollActive)
+
+    //Keep track of where user is, makes current nav link active
+    function scrollActive() {
+        // Scrollamount from user
+        let scrollY = window.pageYOffset;
         
-        let sectionHeight = current.offsetHeight
-        let sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute("id")
-        
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active")
+        sections.forEach(current => {
+            
+            let sectionHeight = current.offsetHeight
+            let sectionTop = current.offsetTop - 50
+            sectionId = current.getAttribute("id")
+            
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active")
+            } else {
+                document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active")
+
+            }
+
+
+        })
+    }
+
+    let checkbox = document.getElementById("checkbox");
+    let body = document.querySelector("body");
+    let checkboxContainer = document.getElementById("label");
+
+    checkbox.addEventListener("change", () => {
+        if(body.classList.contains("original")) {
+            body.classList.remove("original");
+            body.classList.add("contrast");
+
+            checkboxContainer.classList.add("checkbox-background");
+
         } else {
-            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active")
+            body.classList.remove("contrast");
+            body.classList.add("original");
+            checkboxContainer.classList.remove("checkbox-background");
 
-        }
+        } 
+    });
 
-
-    })
-}
-
-let checkbox = document.getElementById("checkbox");
-let body = document.querySelector("body");
-let checkboxContainer = document.getElementById("label");
-
-checkbox.addEventListener("change", () => {
-    if(body.classList.contains("original")) {
-        body.classList.remove("original");
-        body.classList.add("contrast");
-
-        checkboxContainer.classList.add("checkbox-background");
-
-    } else {
-        body.classList.remove("contrast");
-        body.classList.add("original");
-        checkboxContainer.classList.remove("checkbox-background");
-
-    } 
 });
